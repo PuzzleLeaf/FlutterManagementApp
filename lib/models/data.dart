e821028ba28d089df91f55e584be2922895681d4
Puzzleleaf <cmtyx2@naver.com>
2020-05-10 02:12:41 +0900

    project base

---
+++ lib/models/data.dart
@@ -0,0 +1,32 @@
+var memberData = [
+  {
+    'name': 'Milosz Klimek',
+    'image': 'assets/images/1.png',
+    'role': 'UI Designer, Team Leader',
+    'goalRatio': 83
+  },
+  {
+    'name': 'Gabriela Bryndal',
+    'image': 'assets/images/2.png',
+    'role': 'Product Designer',
+    'goalRatio': 81
+  },
+  {
+    'name': 'Mateusz Przytula',
+    'image': 'assets/images/3.png',
+    'role': 'Product Designer',
+    'goalRatio': 78
+  },
+  {
+    'name': 'Patrycja Batko',
+    'image': 'assets/images/4.png',
+    'role': 'Frontend Developer',
+    'goalRatio': 78
+  },
+  {
+    'name': 'Marta Kolbusz',
+    'image': 'assets/images/5.png',
+    'role': 'Quality Assurance',
+    'goalRatio': 79
+  },
+];
